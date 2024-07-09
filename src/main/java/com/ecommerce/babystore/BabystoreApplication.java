7ec5393bb6dca77443637eadd91752508d822dce
Mr Tung Beo <tungpt2@epayjsc.com>
2024-07-09 09:12:50 +0700

    init project

---
+++ src/main/java/com/ecommerce/babystore/BabystoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.babystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabystoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabystoreApplication.class, args);
+    }
+
+}
